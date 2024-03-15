408e33f44775e2d23d62bc85bdfcedeb9bb424b7
Prasanna <me210003054@iiti.ac.in>
2024-03-15 15:56:29 +0530

    added validations

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ connectToMongo();
 const app = express();
 const port = 8080;
 
+app.use(express.json()); //used to get object from GET req (req.body)
+// if we want to use req.body, then this middleware has to be used
 app.use("/api/notes", require("./routes/notes.js"));
 app.use("/api/auth", require("./routes/auth.js"));
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("HIT THE AUTH ROUTE");
-});
+const User = require("../models/User.js");
+// create a user using POST "/api/auth". Doesnt require auth
+const { body, validationResult } = require("express-validator");
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 2 }),
+    body("email", "Should be valid email").isEmail(),
+    body("password", "password must contain at least 5 letters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((error) => {
+        console.log(error);
+        res.json({
+          note: "please enter unique value",
+        });
+      });
+  }
+);
 
 module.exports = router;
