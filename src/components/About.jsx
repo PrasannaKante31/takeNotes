7881126eb3f959f83560806e64a2dce72805c8c5
Prasanna <me210003054@iiti.ac.in>
2024-03-27 17:38:15 +0530

    added frontend components, currently not connected with the database and are serverless

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react'
+import NoteContext  from "../contexts/notes/NoteContext.jsx"
+
+const About = () => {
+  return (
+    <div>
+      <h1>THIS IS ABOUT </h1>
+    </div>
+  )
+}
+
+export default About;
