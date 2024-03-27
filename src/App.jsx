b84ed77fc0b713551fd15f85a3b6e781b2ac23ac
Prasanna <me210003054@iiti.ac.in>
2024-03-27 17:40:55 +0530

    introduced react-dom and created paths for about and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
 import './App.css'
-
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx"
+import NoteState from './contexts/notes/NoteState';
+//Switch in react-router-dom has been depricated and Routes is being used
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+//defining routes has also changed in react-router-dom
+// https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
 function App() {
+    //  wrapping the app inside NoteState, the ones which are inside will be able to access the props 
 
   return (
     <>
-      <h1>takeSomeNotes</h1>
-       
+    <NoteState>
+      <Router>
+        <Navbar/>
+        <div className="container">
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/About' element={<About/>} />
+        </Routes>
+        </div>
+      </Router>
+    </NoteState>
     </>
   )
 }
